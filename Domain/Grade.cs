8f0426a5f2c2b5f9b55abb0f42a19c5d423d8004
Sinkarq2 <82605720+Sinkarq@users.noreply.github.com>
2025-05-27 23:08:04 +0300

    Good

--- Domain/Grade.cs
+++ Domain/Grade.cs
@@ -10,6 +10,9 @@ public sealed class Grade
     public Guid Id { get; set; }
     public string StudentId { get; set; }
     public Student Student { get; set; }
+    public string CreatedByName { get; set; }
+    public string CreatedById { get; set; }
+    public DateTime CreatedOn { get; set; }
     public string Subject { get; set; }
     public int Value { get; set; }
 }
---
+++ SchoolManagement/Controllers/GradeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+using SchoolManagement.Infrastructure;
+
+namespace SchoolManagement.Controllers;
+
+public class GradeController : Controller
+{
+    private readonly ApplicationDbContext dbContext;
+    public GradeController(ApplicationDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    [HttpGet("/Grades")]
+    [CustomAuthorize(Roles = "Student,Teacher,Admin")]
+    public async Task<IActionResult> Grades()
+    {
+        var userRole = this.User.FindFirstValue(ClaimTypes.Role);
+        var list = new List<Domain.Grade>();
+        if (userRole == "Admin")
+        {
+            var grades = await this.dbContext.Grades
+                .Where(x => x.StudentId == this.User.FindFirstValue(ClaimTypes.NameIdentifier))
+                .ToListAsync();
+            list = grades;
+        }
+        
+        if (userRole == "Teacher")
+        {
+            var teacherId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var grades = await this.dbContext.Grades
+                .Where(x => x.CreatedById == teacherId)
+                .ToListAsync();
+            list = grades;
+        }
+        
+        if (userRole == "Student")
+        {
+            var studentId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var grades = await this.dbContext.Grades
+                .Where(x => x.StudentId == studentId)
+                .ToListAsync();
+            list = grades;
+        }
+        
+        return this.View(list);
+    }
+}
